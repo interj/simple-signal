d439523057efe8c8c4eccd4875aa87eb60148892
Jakub Gargul <jakub.gargul@gmail.com>
2021-11-13 18:15:10 +0200

    basic functionality tests

--- tests/Tests.cpp
+++ tests/Tests.cpp
@@ -1,5 +1,6 @@
 #include "../signal.hpp"
 #include <gtest/gtest.h>
+#include <vector>
 
 struct SignalTests: public ::testing::Test
 {
@@ -18,6 +19,18 @@ TEST_F(SignalTests, disconnects)
     EXPECT_TRUE(connection.isConnected());
     connection.disconnect();
     EXPECT_FALSE(connection.isConnected());
+};
+
+TEST_F(SignalTests, disconnectsWhenOutOfScope)
+{
+    bool called = false;
+    {
+        unsigned outOfScopeVariableProtectedByDisconnect;
+        auto connection = sig.connect([&]{called = true; outOfScopeVariableProtectedByDisconnect = -1;});
+        EXPECT_TRUE(connection.isConnected());
+    }
+    sig();
+    EXPECT_FALSE(called);
 };
 
 TEST_F(SignalTests, releases)
@@ -39,7 +52,7 @@ TEST_F(SignalTests, simpleConnsAreMovable)
     EXPECT_TRUE(std::is_move_assignable_v<decltype(freeFuncConn)> && std::is_move_constructible_v<decltype(freeFuncConn)>);
 
     Example ex;
-    auto memberFuncConn = sig.connect(std::bind(&Example::exampleOp,  &ex));
+    auto memberFuncConn = sig.connect(std::bind(&Example::exampleOp, &ex));
     EXPECT_FALSE(std::is_move_assignable_v<decltype(memberFuncConn)> && std::is_move_constructible_v<decltype(memberFuncConn)>);
 
     int localVar;
@@ -64,6 +77,34 @@ TEST_F(SignalTests, callsAreIgnored)
     sig();
     EXPECT_EQ(timesCalled, 2);
 
+};
+
+TEST_F(SignalTests, typeErasedSignalsWork)
+{
+    std::vector<TypeErasedSignal> signals;
+
+    bool voidSigCalled = false;
+    auto voidSigConn = sig.connect([&voidSigCalled]{voidSigCalled = true;});
+    signals.push_back(std::move(sig));
+    sig();
+    EXPECT_FALSE(voidSigCalled);
+
+    bool intSigCalled = false;
+    signals.push_back(Signal<void(int)>{});
+    auto intSigConn = static_cast<Signal<void(int)>&>(signals.back()).connect([&intSigCalled](int){intSigCalled = true;});
+
+    bool boolSigCalled = false;
+    signals.push_back(Signal<void(bool&)>{});
+    auto boolSigConn = static_cast<Signal<void(bool&)>&>(signals.back()).connect([](bool& called){called = true;});
+
+    static_cast<Signal<void()>&>(signals[0])();
+    EXPECT_TRUE(voidSigCalled);
+
+    std::invoke(static_cast<Signal<void(int)>&>(signals[1]), 42);
+    EXPECT_TRUE(intSigCalled);
+
+    static_cast<Signal<void(bool&)>&>(signals[2]).operator()(boolSigCalled);
+    EXPECT_TRUE(boolSigCalled);
 };
 
 
